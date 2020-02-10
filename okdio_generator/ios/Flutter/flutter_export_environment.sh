6b4a008b8bec6d490e73cbafc55192a1cde92940
Administrator <274465032@qq.com>
2020-02-10 17:02:06 +0800

    change dio

---
+++ okdio_generator/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/chenjian/development/flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/chenjian/workspace/opensource/flutter/based_on_chopper/okdio_generator"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/chenjian/development/flutter/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=0.0.2"
+export "FLUTTER_BUILD_NUMBER=0.0.2"
